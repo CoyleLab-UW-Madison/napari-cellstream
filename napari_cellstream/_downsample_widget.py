f6c27753c147280bb24546b7be36d4d158ace6d6
eweix <elliott.weix@icloud.com>
2025-08-11 13:11:55 -0500

    ci: ruff

--- napari_cellstream/_downsample_widget.py
+++ napari_cellstream/_downsample_widget.py
@@ -1,12 +1,9 @@
-from magicgui import magicgui
-from napari.types import ImageData
-from napari.layers import Image
-from napari import current_viewer
-from qtpy.QtWidgets import QWidget
 import torch
-import numpy as np
+from cellstream.image import downsample
+from magicgui import magicgui
+from napari import current_viewer
+from napari.layers import Image
 
-from cellstream.image import downsample  
 
 @magicgui(
     call_button="Downsample active image",
@@ -15,7 +12,6 @@ def downsample_gui_widget(
     downsample_by: float = 1,
     is_mask: bool = False,
 ):
-    
     viewer = current_viewer()
     if viewer is None:
         raise RuntimeError("No active napari viewer found")
@@ -25,14 +21,10 @@ def downsample_gui_widget(
         raise RuntimeError("No active image layer selected")
 
     img = layer.data
-    img = torch.from_numpy(img.astype('float32'))
- 
-    print("Genearting color-coded spectra...")
-    ds = downsample(
-        tensor=img,
-        scale=downsample_by,
-        is_mask=is_mask
-    )
+    img = torch.from_numpy(img.astype("float32"))
 
-    ds=ds.detach().numpy()
+    print("Genearting color-coded spectra...")
+    ds = downsample(tensor=img, scale=downsample_by, is_mask=is_mask)
+
+    ds = ds.detach().numpy()
     return ds
